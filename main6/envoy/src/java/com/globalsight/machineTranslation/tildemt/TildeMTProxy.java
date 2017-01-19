0cfea3d2b249c10c559d4ecff2da7f22cbd078c0
Rihards Krišlauks <rihards.krislauks@tilde.lv>
2017-01-19 17:56:19 +0200

    Add initial support for re-nesting tags

--- main6/envoy/src/java/com/globalsight/machineTranslation/tildemt/TildeMTProxy.java
+++ main6/envoy/src/java/com/globalsight/machineTranslation/tildemt/TildeMTProxy.java
@@ -3,13 +3,27 @@ package com.globalsight.machineTranslation.tildemt;
 import com.globalsight.everest.projecthandler.MachineTranslationProfile;
 import com.globalsight.everest.webapp.pagehandler.administration.mtprofile.MTProfileConstants;
 import com.globalsight.machineTranslation.AbstractTranslator;
+import com.globalsight.machineTranslation.MTHelper;
 import com.globalsight.machineTranslation.MachineTranslationException;
 import com.globalsight.machineTranslation.MachineTranslator;
+import com.globalsight.util.StringUtil;
+import com.globalsight.util.edit.GxmlUtil;
+import com.globalsight.util.gxml.GxmlElement;
+import com.globalsight.util.gxml.GxmlReader;
+import com.globalsight.util.gxml.TextNode;
 import org.apache.http.NameValuePair;
 import org.apache.http.message.BasicNameValuePair;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.w3c.dom.*;
+import org.xml.sax.SAXException;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
 import java.net.URISyntaxException;
 import java.util.*;
 
@@ -62,6 +76,7 @@ public class TildeMTProxy extends AbstractTranslator {
     @Override
     protected String doTranslation(Locale p_sourceLocale, Locale p_targetLocale, String p_string)
             throws MachineTranslationException {
+        String processed = preprocessTags(p_string);
         Map paramMap = getMtParameterMap();
         String json = (String) paramMap.get(MTProfileConstants.MT_TILDEMT_STATE_JSON);
         JSONObject obj;
@@ -82,7 +97,7 @@ public class TildeMTProxy extends AbstractTranslator {
         } catch (URISyntaxException e){
             throw new MachineTranslationException(e);
         }
-        TranslateResult res = service.Translate(p_string, params.requestParams);
+        TranslateResult res = service.Translate(processed, params.requestParams);
 
         Double qeThreshold;
         try {
@@ -91,7 +106,129 @@ public class TildeMTProxy extends AbstractTranslator {
             qeThreshold = null;
         }
 
-        return qeThreshold == null || res.QeScore >= qeThreshold ? res.Translation : null;
+        String translation;
+        try {
+             translation = restoreTags(p_string, res.Translation);
+        } catch (SAXException | ParserConfigurationException | IOException e){
+            translation = res.Translation;
+        }
+
+        return qeThreshold == null || res.QeScore >= qeThreshold ? translation : null;
+    }
+
+    private String preprocessTags(String original)
+            throws MachineTranslationException {
+        GxmlElement gxmlRoot = MTHelper.getGxmlElement(original);
+        List childElements = gxmlRoot.getChildElements();
+        StringBuilder preprocessed = new StringBuilder();
+        for (Object item :
+                childElements) {
+            GxmlElement element = (GxmlElement)item;
+            switch (element.getType()){
+                case GxmlElement.TEXT_NODE:
+                    preprocessed.append(element.getTextNodeValue());
+                    break;
+                case GxmlElement.BPT: {
+                    String i = getElementAttribute(element, "i");  // functions similar to "id" for BPT and EPT
+                    // we assume that there always will be a corresponding EPT tag
+                    // that will provide a closing </span> tag; and that BPT does
+                    // not have any translatable content inside it
+                    preprocessed.append(String.format("<span id=\"%1$s\" type=\"%2$s\">",
+                            i, GxmlElement.BPT));
+                    break;
+                }
+                case GxmlElement.EPT:
+                    // we assume that always when EPT is encountered there has been a
+                    // corresponding BPT before that for which an opening <span> tag
+                    // has already been placed; and that EPT does not have any
+                    // translatable content inside it
+                    preprocessed.append("</span>");
+                    break;
+                default: {
+                    String id = getElementAttribute(element, "id");
+                    // we assume that the element doesn't have any translatable content
+                    preprocessed.append(String.format("<span id=\"%1$s\" type=\"%2$s\"/>",
+                            id, element.getType()));
+                    break;
+                }
+
+            }
+        }
+        return "<html>" + preprocessed.toString() + "</html>";
+    }
+
+    private String getElementAttribute(GxmlElement element, String attribute) throws MachineTranslationException{
+        String id = element.getAttribute(attribute);
+        if (id == null) {
+            throw new MachineTranslationException(
+                    String.format("The GXML node doesn't contain an attribute called '%1$s' - %2$s",
+                            attribute, element.toString()));
+        }
+        return id;
+    }
+
+    private String restoreTags(String original, String translation)
+            throws ParserConfigurationException,
+            IOException,
+            SAXException,
+            MachineTranslationException{
+        StringBuilder restored = new StringBuilder();
+        GxmlElement gxmlRoot = MTHelper.getGxmlElement(original);
+        DocumentBuilderFactory factory =
+                DocumentBuilderFactory.newInstance();
+        DocumentBuilder builder = factory.newDocumentBuilder();
+        ByteArrayInputStream input =  new ByteArrayInputStream(
+                translation.getBytes("UTF-8"));
+        Document doc = builder.parse(input);
+        NodeList rootNodes = doc.getChildNodes();
+        if (rootNodes.getLength() != 1){
+            throw new MachineTranslationException("Invalid XML received from LetsMT");
+        }
+        NodeList translationNodes = rootNodes.item(0).getChildNodes();
+        for (int i=0; i < translationNodes.getLength(); i++){
+            Node current = translationNodes.item(i);
+            switch (current.getNodeType()) {
+                case Node.TEXT_NODE:
+                    restored.append(current.getTextContent());
+                    break;
+                case Node.ELEMENT_NODE:
+                    String content = current.getTextContent();
+                    Element element = (Element) current;
+                    String id = element.getAttribute("id");
+                    String type = element.getAttribute("type");
+                    if (type.equals(Integer.toString(GxmlElement.BPT))) {
+                        // means that a <bpt> and an <ept> tag was replaced
+                        // with a single <span> tag with the same id
+
+                        // for BPT and EPT "i" functions similar to "id"
+                        GxmlElement bpt = gxmlRoot.getDescendantByAttributeValue("i", id, GxmlElement.BPT);
+                        GxmlElement ept = gxmlRoot.getDescendantByAttributeValue("i", id, GxmlElement.EPT);
+                        restored.append(bpt.toGxml());
+                        if(content != null) {
+                            restored.append(content);
+                        }
+                        restored.append(ept.toGxml());
+                    } else {
+                        // means that a <ph/> or some other tag was replaced with a
+                        // <span> tag with the same id
+                        // we assume that there hasn't been any translatable content
+                        // inside the tag
+                        Integer typeInt = Integer.parseInt(type);
+                        GxmlElement node = gxmlRoot.getDescendantByAttributeValue("id", id, typeInt);
+                        restored.append(node.toGxml());
+                    }
+                    break;
+                default:
+                    // this should not happen
+                    break;
+            }
+        }
+
+        int index = original.indexOf(">");
+        String head = original.substring(0, index + 1);
+        String tail = "</segment>";
+
+        return head + restored.toString() + tail;
     }
 
     private ServiceParams getParamsFromJson(Locale p_sourceLocale, Locale p_targetLocale, JSONObject obj)
